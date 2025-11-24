bf63ba0332f279b0567766778d62c0255e2b63f8
Rume Sefia <rumesefia@outlook.com>
2025-11-24 18:47:47 +0100

    Update typing_stats.py

--- typing_stats.py
+++ typing_stats.py
@@ -3,6 +3,12 @@ from dataclasses import dataclass
 
 @dataclass
 class TypingStats:
+    """
+    A dataclass containing the final stats for the typing speed test.
+    The class calculates the words per minute (wpm) and the characters per second(cps) as properties.
+    Also creates the summary displayed at the end of the test
+    """
+    #Variables
     characters_typed : int = 0
     words_typed : int = 0
     time_spent : float = 0
@@ -10,14 +16,17 @@ class TypingStats:
 
     @property
     def wpm(self) -> float:
+        """Words per minute calculated from the updated statistic variables"""
         return (self.words_typed/self.time_spent) *60 if self.time_spent >0 else 0.00
 
     @property
     def cps(self) -> float:
+        """Average characters typed per second. Calculated from updated variable"""
         return (self.characters_typed/self.time_spent) if self.time_spent >0 else 0.00
 
     @property
     def summary(self) -> str:
+        """Returns the string containing the stat summary for information label and final display"""
         return (f"Typing Stats:\n"
                 f"Characters Per Second (CPS): {self.cps:.2f}\n"
                 f"Words Per Minute (WPM): {self.wpm:.2f}\n"
